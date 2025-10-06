19f742937feee5648cd1b090333d449d77b90f54
José Yáñez <joseyanez2298@gmail.com>
2025-10-06 12:49:57 -0500

    Inicio del proyecto

---
+++ src/main/java/com/joseyaniez/tapanaranja/TapanaranjaApplication.java
@@ -0,0 +1,13 @@
+package com.joseyaniez.tapanaranja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapanaranjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapanaranjaApplication.class, args);
+	}
+
+}
