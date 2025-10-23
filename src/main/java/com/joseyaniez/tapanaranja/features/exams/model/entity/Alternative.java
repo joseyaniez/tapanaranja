ce6bcdc4a43bae7046c815c2ae4b3af2a1af7a28
José Yáñez <joseyanez2298@gmail.com>
2025-10-22 19:32:40 -0500

    db: creación de base de datos y conexión

--- src/main/java/com/joseyaniez/tapanaranja/features/exams/model/entity/Alternative.java
+++ src/main/java/com/joseyaniez/tapanaranja/features/exams/model/entity/Alternative.java
@@ -8,15 +8,21 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Alternative
  */
+@Entity
+@NoArgsConstructor
+@Data
 public class Alternative {
 
     @Id
--- src/main/java/com/joseyaniez/tapanaranja/features/students/controller/CategoryController.java
+++ src/main/java/com/joseyaniez/tapanaranja/features/students/controller/CategoryController.java
@@ -1,6 +1,7 @@
 
 package com.joseyaniez.tapanaranja.features.students.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class CategoryController {
         this.categoryService = categoryServiceImpl;
     }
 
+    @PostMapping
     public CategoryResponse createCategory(@RequestBody @Valid CategoryRequest categoryRequest){
         CategoryResponse categoryResponse = categoryService.createCategory(categoryRequest);
         return categoryResponse;
