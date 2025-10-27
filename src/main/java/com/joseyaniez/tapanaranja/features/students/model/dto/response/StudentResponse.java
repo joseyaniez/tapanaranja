69a049823efd860a749df08e4b97c78a6b17212a
José Yáñez <joseyanez2298@gmail.com>
2025-10-27 17:43:18 -0500

    service: StudentResponse in service

---
+++ src/main/java/com/joseyaniez/tapanaranja/features/students/model/dto/response/StudentResponse.java
@@ -0,0 +1,12 @@
+
+package com.joseyaniez.tapanaranja.features.students.model.dto.response;
+
+/**
+ * StudentResponse
+ */
+public record StudentResponse(
+    Long id,
+    String username,
+    Long category_id
+) {
+}
--- src/main/java/com/joseyaniez/tapanaranja/features/students/service/StudentService.java
+++ src/main/java/com/joseyaniez/tapanaranja/features/students/service/StudentService.java
@@ -2,14 +2,14 @@
 package com.joseyaniez.tapanaranja.features.students.service;
 
 import com.joseyaniez.tapanaranja.features.students.model.dto.request.StudentRequest;
-import com.joseyaniez.tapanaranja.features.students.model.entity.Student;
+import com.joseyaniez.tapanaranja.features.students.model.dto.response.StudentResponse;
 
 /**
  * StudentService
  */
 public interface StudentService {
-    Student getById(Long id);
-    Student create(StudentRequest studentRequest);
-    Student update(Long id, StudentRequest studentRequest);
+    StudentResponse getById(Long id);
+    StudentResponse create(StudentRequest studentRequest);
+    StudentResponse update(Long id, StudentRequest studentRequest);
     void delete(Long id);
 }
